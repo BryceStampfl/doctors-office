25222f423f820edca15a2debada8b0ea310aab29
Bryce Stampfl <brycestampfl@gmail.com>
2020-06-23 09:55:22 -0700

    added a hover directive to change the color of an element when mouseover

---
+++ AngularClient/src/app/directives/hover.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appHover]'
+})
+export class HoverDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('lightblue');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
