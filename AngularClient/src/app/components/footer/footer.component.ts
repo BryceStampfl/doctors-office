3388d94edfd00cecea6e81e553ea383c3c89acd4
Bryce Stampfl <brycestampfl@gmail.com>
2020-06-30 17:53:31 -0700

    added footer

---
+++ AngularClient/src/app/components/footer/footer.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {FooterService} from '../../services/footer.service';
+import {FooterLinks} from '../../models/footer-links.model';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  footers: FooterLinks[];
+
+  constructor(private footerService: FooterService) { }
+
+  ngOnInit(): void {
+    this.footers = this.footerService.getFooters();
+  }
+
+}
